ffc66fb08ec6f55f9d4330d302bb84f5b8315714
AviralMishra1310 <aviralmishra131005@gmail.com>
2026-07-08 16:02:23 +0530

    Setup React frontend using Vite

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>OfferPrep</h1>
+      <p>AI Powered Interview Preparation Platform</p>
+    </div>
+  );
+}
+
+export default App;
