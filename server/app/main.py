062c2351267be115cca9f8a21f99fa0e6cd5ba02
AviralMishra1310 <aviralmishra131005@gmail.com>
2026-07-10 13:55:15 +0530

    Setup authentication router

--- server/app/main.py
+++ server/app/main.py
@@ -4,11 +4,14 @@ from sqlalchemy import text
 from app.core.config import settings
 from app.database.database import engine
 
+from app.routers.auth import router as auth_router
+
+
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION,
 )
-
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
