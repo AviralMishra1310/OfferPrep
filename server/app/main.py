77b70cf1088cdf3dcc07c078dc6b49a08ca9a224
AviralMishra1310 <aviralmishra131005@gmail.com>
2026-07-10 14:40:08 +0530

    Create user registration API

--- server/app/main.py
+++ server/app/main.py
@@ -3,15 +3,15 @@ from sqlalchemy import text
 
 from app.core.config import settings
 from app.database.database import engine
-
 from app.routers.auth import router as auth_router
-
 
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION,
 )
+
 app.include_router(auth_router)
+
 
 @app.get("/")
 def home():
--- server/app/routers/auth.py
+++ server/app/routers/auth.py
@@ -1,10 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-router = APIRouter(prefix="/auth", tags=["Authentication"])
+from app.database.dependencies import get_db
+from app.models.user import User
+from app.schemas.user import UserRegister
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
 
 
-@router.get("/")
-def test():
+@router.post("/register")
+def register(user: UserRegister, db: Session = Depends(get_db)):
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password=user.password,
+    )
+
+    db.add(new_user)
+    db.commit()
+
     return {
-        "message": "Authentication Router Working"
+        "message": "User Registered Successfully"
     }
